abfe8c7e3906de77f145d803e8942ce34d0d5c51
Keanna Salter <keannasalter91@gmail.com>
2020-02-03 02:45:09 +0000

    Done.

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -1,8 +1,10 @@
-winner = ""
-passengers.each do |suite,name|
-    if suite == :suite_a && name.start_with?("A")
-      winner = name
-    end
-  
-  winner
+def select_winner(passengers)
+  winner = ""
+passengers.each do |suite, name|
+  if suite == :suite_a && name.start_with?("A")
+    winner = name
+  end
+end
+ 
+winner
 end
